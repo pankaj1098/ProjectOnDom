6ae5cd5c0576dc34f56a8eddd068b94c5b1a6920
pankaj1098 <pankajyadav.ce@gmail.com>
2022-11-10 23:30:00 +0530

    task 8

---
+++ main.js
@@ -0,0 +1,7 @@
+function saveToLocalStorage(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const email=event.target.email.value;
+    localStorage.setItem('name',name);
+    localStorage.setItem('email',email);
+}
