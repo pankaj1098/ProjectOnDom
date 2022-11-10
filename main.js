d6330907e7b044aab631d46f42a4bbbfe619f49c
pankaj1098 <pankajyadav.ce@gmail.com>
2022-11-11 02:14:58 +0530

    task11localstorage

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function saveToLocalStorage(event){
     event.preventDefault();
     const name=event.target.name.value;
     const email=event.target.email.value;
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
+    const obj = {
+        name,
+        email,
+    }
+    localStorage.setItem(email, JSON.stringify(obj));
 }
+
+
